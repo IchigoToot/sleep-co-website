b469f18de334d0a22be9b2f5c09fd93009c026ad
IchigoToot <100898791+IchigoToot@users.noreply.github.com>
2022-05-17 12:57:34 +1000

    adding popup image section

--- _site/script.js
+++ _site/script.js
@@ -8,4 +8,18 @@ $( document ).ready(function() {
 
     // Animate On Scroll-Activate
     AOS.init();
+      duration: 500 // values from 0 to 3000,with step 50ms
+});
+
+
+lightGallery(document.getElementById('lightgallery'), {
+    speed: 500,
+    download: false
+    
+});
+
+lightGallery-2(document.getElementById('lightgallery-2'), {
+    speed: 500,
+    download: false
+    
 });
